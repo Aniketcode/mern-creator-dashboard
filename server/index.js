bed416fc045e02e8733ed7cc9eb97aa946f01e8b
Aniket <mailsinghaniket@gmail.com>
2025-04-27 21:41:39 +0530

    cors error fixing

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ const corsOptions = {
     credentials:true
   }
 app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 // Routes
 app.use('/api/user', userRoute);
 app.use('/api/feed', feedRoute);
