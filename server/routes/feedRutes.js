ddfde2b47b24919a7a98705381481a7546a918ce
Aniket <mailsinghaniket@gmail.com>
2025-04-28 10:02:28 +0530

    added user agent to redit feed api

--- server/routes/feedRutes.js
+++ server/routes/feedRutes.js
@@ -11,7 +11,7 @@ import {
 const router = express.Router();
 
 // @api/feed/aggregate
-router.get('/aggregate', getAggregatedFeed);
+router.get('/aggregate', authProtect, getAggregatedFeed);
 
 // @api/feed/save
 router.post('/save', authProtect, savePost);
