6a30bf25c11c58f27ff7176c16913274529e3c5c
Aniket <mailsinghaniket@gmail.com>
2025-04-28 09:35:30 +0530

    add deploy link to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
+  base : "./",
+  server : {
+    proxy : {
+      '/api' :{
+        target : import.meta.env.VITE_BACKEND_URL,
+        changeOrigin : true,
+        secure:true
+      }
+    }
+  }
 })
